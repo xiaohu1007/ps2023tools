1d7d395a7a922ff983916f1b6c301f88d7b6f820
吴昊虎 <wuhaohu@zhangzhongyun.com>
2025-03-28 18:10:47 +0800

    fixbug: 修复路由刷新问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@views/home/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/ps2023tools/'),
   routes: [
     {
       path: '/',
